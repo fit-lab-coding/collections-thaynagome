6b25223042fc8d4845d38ae43d01f986e21d3f16
Thayna Gomes <tluzsantos@hotmail.com>
2021-02-03 22:52:49 -0300

    aluno

--- src/test/java/fit/AppTest.java
+++ src/test/java/fit/AppTest.java
@@ -1,19 +1,14 @@
 package fit;
 
-import static org.junit.Assert.assertArrayEquals;
 import static org.junit.Assert.assertEquals;
 
-import java.util.ArrayList;
 import java.util.Collection;
-import java.util.Comparator;
 import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
 
 import org.junit.Test;
 
 import fit.core.AttendanceHelper;
-import fit.core.ListUtils;
 
 /**
  * Unit test for simple App.
@@ -22,38 +17,38 @@ public class AppTest {
     /**
      * validate if class can sort an regular array
      */
-    @Test
-    public void shouldSortList() {
-        // arrange
-        List<String> names = new ArrayList<>() {
-            {
-                add("Emilio");
-                add("Maria");
-                add("José");
-            }
-        };
-
-        List<String> expectedResult = new ArrayList<>() {
-            {
-                add("Emilio");
-                add("José");
-                add("Maria");
-            }
-        };
-
-        // comparator
-        Comparator<String> comp = (String one, String another) -> {
-            if (one == null)
-                return -1;
-            return (one.compareTo(another));
-        };
-
-        // act
-        List<String> actualResult = ListUtils.<String>sort(names, comp);
-
-        // assert
-        assertArrayEquals(expectedResult.toArray(), actualResult.toArray());
-    }
+//    @Test
+//    public void shouldSortList() {
+//        // arrange
+//        List<String> names = new ArrayList<>() {
+//            {
+//                add("Emilio");
+//                add("Maria");
+//                add("José");
+//            }
+//        };
+//
+//        List<String> expectedResult = new ArrayList<>() {
+//            {
+//                add("Emilio");
+//                add("José");
+//                add("Maria");
+//            }
+//        };
+//
+//        // comparator
+//        Comparator<String> comp = (String one, String another) -> {
+//            if (one == null)
+//                return -1;
+//            return (one.compareTo(another));
+//        };
+//
+//        // act
+//        List<String> actualResult = ListUtils.<String>sort(names, comp);
+//
+//        // assert
+//        assertArrayEquals(expectedResult.toArray(), actualResult.toArray());
+//    }
 
     @Test
     public void shouldGenerateRandomAttendanceList() {
@@ -70,7 +65,7 @@ public class AppTest {
         //assert
 
         //same size (due to parameters sent to random method)
-        assertEquals(attendanceListResult.size(), attendanceListResult.size());
+        assertEquals(attendanceListResult.size(), attendanceListExpected.size());
 
         //same percentile of 'true' values due to parameters sent to random method
         assertEquals(
